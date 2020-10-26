88917c14b6208195588a721c76dc32e486a326cf
Wojciech Iskra <wojtek@domeq.net>
2020-10-26 13:20:03 +0100

    Initial version of the middleware, to be developed further (#1)
    Co-authored-by: Wojciech Iskra <wojciech.iskra@schibsted.com>

---
+++ index.js
@@ -0,0 +1,77 @@
+const R = require('ramda');
+
+const ignoreMethods = ['post', 'put', 'patch', 'delete'];
+const cacheHeaders = ['expires', 'cache-control'];
+
+const cacheHeadersAlreadySet = (response) =>
+    R.pipe(R.propOr([], 'headers'), R.pick(cacheHeaders), R.filter(Boolean), R.keys)(response).length > 0;
+
+const generateCacheHeaders = (routeSettings, currentHeaders = {}) => {
+    const generatedHeaders = currentHeaders;
+
+    if (routeSettings !== false) {
+        // First check if we have client and varnish max age defined in the route config
+        // If we don't have, see if we have standard maxAge options, if not fallback to server defaults
+        const cacheControl = {
+            maxAge: R.prop('clientTime', routeSettings),
+            sMaxAge: R.prop('serverTime', routeSettings),
+            directive: R.propOr('', 'directive', routeSettings),
+        };
+
+        if (R.isNil(cacheControl.maxAge)) {
+            return generatedHeaders;
+        }
+
+        // max-age is how long the client will cache the response
+        // s-maxage is how long varnish will cache the response
+        if (!currentHeaders['cache-control']) {
+            generatedHeaders['cache-control'] = `max-age=${cacheControl.maxAge},s-maxage=${cacheControl.sMaxAge}${
+                cacheControl.directive ? `,${cacheControl.directive}` : ''
+            }`;
+        }
+
+        if (!currentHeaders['surrogate-control']) {
+            generatedHeaders['surrogate-Control'] = `max-age=${cacheControl.sMaxAge}`;
+        }
+    } else {
+        generatedHeaders['cache-control'] = 'no-cache, no-store';
+    }
+
+    return generatedHeaders;
+};
+
+const shouldAddCachingHeaders = (handler) => {
+    if (ignoreMethods.includes(handler.event.httpMethod.toLowerCase())) {
+        return false;
+    }
+
+    // Don't touch headers if something else has already set them
+    if (cacheHeadersAlreadySet(handler.response)) {
+        return false;
+    }
+
+    return true;
+};
+
+const cachingMiddleware = ({ success = {}, errors = {} } = {}) => ({
+    after: async (handler) => {
+        if (!shouldAddCachingHeaders(handler)) {
+            return;
+        }
+
+        // eslint-disable-next-line no-param-reassign
+        handler.response.headers = generateCacheHeaders(success, handler.response.headers);
+    },
+    onError: async (handler) => {
+        if (!shouldAddCachingHeaders(handler)) {
+            return;
+        }
+
+        const errorCacheConfiguration = R.prop(handler.response.statusCode, errors);
+
+        // eslint-disable-next-line no-param-reassign
+        handler.response.headers = generateCacheHeaders(errorCacheConfiguration, handler.response.headers);
+    },
+});
+
+module.exports = cachingMiddleware;
